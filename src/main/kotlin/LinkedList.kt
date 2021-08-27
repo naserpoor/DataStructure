f89805da97c76bd1ebabf6cbaaf0f5ad41e4f9cb
Alireza <anaserpoor@yahoo.com>
2021-08-27 13:32:28 +0430

    add getfirst and getlast

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -2,12 +2,32 @@ class LinkedList<T> {
     private var top:Node<T>? = null
     private class Node<T>(var data:T,var next:Node<T>? = null)
     var size = 0
-        get
         private set
 
     fun add(data:T):Boolean{
-        top = Node(data,top)
+        val newNode = Node(data)
+        val lastNode = getLastNode()
+        lastNode?.next = newNode
+        if (top == null) {
+            top = newNode
+        }
         size++
         return true
+    }
+
+    private fun getFirstNode() = top
+    private fun getLastNode():Node<T>? {
+        var cur = top ?: return null
+        var next = cur.next
+        while (next != null) {
+            cur = next
+            next = cur.next
+        }
+        return cur
+    }
+
+    fun getFirst():T? = getFirstNode()?.data
+    fun getLast():T? {
+        return getLastNode()?.data
     }
 }
--- src/test/kotlin/LinkedListTest.kt
+++ src/test/kotlin/LinkedListTest.kt
@@ -1,14 +1,41 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.RepetitionInfo
 import org.junit.jupiter.api.Test
 
 internal class LinkedListTest {
+    lateinit var list:LinkedList<Int>
+
+    @BeforeEach
+    fun init(){
+        list = LinkedList()
+    }
 
     @Test
     fun `should increase size when one item added`(){
-        val list = LinkedList<Int>()
         list.add(0)
-
         assertEquals(1,list.size)
+
+        list.add(1)
+        assertEquals(2,list.size)
+    }
+
+    @RepeatedTest(5)
+    fun `getFirst should return First Item`(info:RepetitionInfo){
+        list.add(info.currentRepetition)
+        list.add(1)
+
+        assertEquals(info.currentRepetition,list.getFirst())
+    }
+
+    @RepeatedTest(5)
+    fun `getLast should return Last Item`(info:RepetitionInfo){
+        list.add(0)
+        list.add(1)
+        list.add(info.currentRepetition)
+
+        assertEquals(info.currentRepetition,list.getLast())
     }
 
 }
